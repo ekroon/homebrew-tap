cb04202c70f7b8591387ac19f48780a6d4946da9
Erwin Kroon <erwin@k3n.nl>
2022-09-08 22:52:35 +0200

    spark-rs 0.1.0 (new formula)
    spark-rs v0.1.0
    
    Closes #1.
    
    Signed-off-by: Erwin Kroon <123574+ekroon@users.noreply.github.com>

---
+++ Formula/spark-rs.rb
@@ -0,0 +1,26 @@
+class SparkRs < Formula
+  desc "Spark clone"
+  homepage "https://github.com/ekroon/spark-rs"
+  url "https://github.com/ekroon/spark-rs/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "990921776c85668e106b5cec02a635d47a199470aa51871f3fbb98f041a36406"
+  license ""
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test spark-rs`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    assert_equal "▁▄█", shell_output("#{bin}/spark 1 2 3").strip
+  end
+end
