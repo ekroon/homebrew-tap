bb45d48909e3442de25cb524e025bc9e82004d6a
Erwin Kroon <123574+ekroon@users.noreply.github.com>
2022-09-08 22:11:39 +0000

    spark-rs: add 0.2.0 bottle.

--- Formula/spark-rs.rb
+++ Formula/spark-rs.rb
@@ -5,6 +5,12 @@ class SparkRs < Formula
   sha256 "3ab0b35f51d80c346ab106cbcff98995276a03c0446012ca781081bd3c56c57f"
   license ""
 
+  bottle do
+    root_url "https://github.com/ekroon/homebrew-tap/releases/download/spark-rs-0.2.0"
+    sha256 cellar: :any_skip_relocation, big_sur:      "883bbec0caa542ae64e0918bc3e233331ed2982bab3551cd3e41dd3cd53e5279"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "f9d87b39f680b5c6c2a5edf5e04fdd082359ef69d703780e0b805afae778505a"
+  end
+
   depends_on "rust" => :build
 
   def install
