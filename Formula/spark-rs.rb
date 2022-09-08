efa4a292beb3345f138bc772d817572614e295db
Erwin Kroon <erwin@k3n.nl>
2022-09-08 23:51:47 +0200

    spark-rs 0.2.0
    Closes #2.
    
    Signed-off-by: Erwin Kroon <123574+ekroon@users.noreply.github.com>

--- Formula/spark-rs.rb
+++ Formula/spark-rs.rb
@@ -1,15 +1,9 @@
 class SparkRs < Formula
   desc "Spark clone"
   homepage "https://github.com/ekroon/spark-rs"
-  url "https://github.com/ekroon/spark-rs/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "990921776c85668e106b5cec02a635d47a199470aa51871f3fbb98f041a36406"
+  url "https://github.com/ekroon/spark-rs/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "3ab0b35f51d80c346ab106cbcff98995276a03c0446012ca781081bd3c56c57f"
   license ""
-
-  bottle do
-    root_url "https://github.com/ekroon/homebrew-tap/releases/download/spark-rs-0.1.0"
-    sha256 cellar: :any_skip_relocation, big_sur:      "5fb53feca04c3c41c4464d2451c95c58de1a298f947eff6ad3c03e7ded5a67b6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "d5c9b050648290f99cda4d6981b7d70d32d0fb52d4ebdb12fe2dc56330e6d631"
-  end
 
   depends_on "rust" => :build
 
